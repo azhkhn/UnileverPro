620860b3739b95d98b7cf3d16fa8463c5574de53
Vuthea Chheang <chheang.vuthea@gmail.com>
2015-11-28 23:15:24 +0700

    filemanager

--- public/responsivefilemanager/filemanager/config/config.php
+++ public/responsivefilemanager/filemanager/config/config.php
@@ -66,7 +66,7 @@ $config = array(
 	| with start and final /
 	|
 	*/
-	'upload_dir' => '/smartmartpro/public/upload/',
+	'upload_dir' => '/public/upload/',
 
 	/*
 	|--------------------------------------------------------------------------
